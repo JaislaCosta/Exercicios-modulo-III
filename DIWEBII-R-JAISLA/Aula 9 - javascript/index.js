afdcf8aae9f7255f1b907639f42dd72be0fa9648
JaislaCosta <jaislacosta2@gmail.com>
2025-09-24 20:29:57 -0300

    Add files via upload

---
+++ DIWEBII-R-JAISLA/Aula 9 - javascript/index.js
@@ -0,0 +1,14 @@
+// console.log('Jaisla Costa Barbosa');
+// console.log('Jaisla "Costa" "Barbosa"');
+// console.log(`Jaisla "Costa" 'Barbosa'`);
+// console.log(123456789);
+
+// console.log(`Meu nome é "Jaísla", estou aprendendo javascript agora, e são exatamente 20:09, 'da noite'`);
+
+// Tipos de variáveis
+// var - está em desuso
+let nome = 'Jaísla';
+nome = 'Aylla'
+console.log(nome);
+// const
+
