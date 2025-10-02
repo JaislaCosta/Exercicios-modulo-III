384027660a9a387fd4cb8f99830f186898ada1a3
JaislaCosta <jaislacosta2@gmail.com>
2025-10-01 21:00:44 -0300

    Add files via upload

--- DIWEBII-R-JAISLA/Aula 9 - javascript/index.js
+++ DIWEBII-R-JAISLA/Aula 9 - javascript/index.js
@@ -46,18 +46,118 @@
 // (IMC é de 22.2)
 // (Jaísla Costa Barbosa nasceu em ----)
 
-const nome = 'Jaísla';
-const sobreNome = ' Costa Barbosa ';
-const idade = 20;
-const peso = 57;
-const altura = 1.60;
-let imc; //peso / (altura * altura)
-let dataNascimento;
+// const nome = 'Jaísla';
+// const sobreNome = ' Costa Barbosa ';
+// const idade = 20;
+// const peso = 57;
+// const altura = 1.60;
+// let imc; //peso / (altura * altura)
+// let dataNascimento;
 
-imc = peso / (altura * altura);
-console.log(imc);
-dataNascimento = 2025 - idade
-console.log(dataNascimento);
+// imc = peso / (altura * altura);
+// console.log(imc);
+// dataNascimento = 2025 - idade
+// console.log(dataNascimento);
 
-console.log(`${nome} ${sobreNome} tem  ${idade}  anos`);
-console.log(`e pesa  ${peso}  kg`);
+// console.log(`${nome} ${sobreNome} tem  ${idade}  anos`);
+// console.log(`e pesa  ${peso}  kg`);
+
+// var nome_aluno = 'Jaísla';
+// var idade = 20;
+// var curso = 'TI';
+
+// document.write(`A aluna ${nome_aluno}, tem ${idade} anos e cursa a disciplina ${curso}.`);
+
+
+
+// Operações
+// var numeroUm = 10;
+// var numeroDois = 5;
+
+// var soma = numeroUm + numeroDois;
+// document.write(`O valor da soma é: ${soma} <br>`);
+
+// var subtracao = numeroUm - numeroDois;
+// document.write(`O valor da subtração é: ${subtracao} <br>`);
+
+// var divisao = numeroUm / numeroDois;
+// document.write(`O valor da divisão é: ${divisao} <br>`);
+
+// var multiplicacao = numeroUm * numeroDois;
+// document.write(`O valor da multiplicação é: ${multiplicacao} <br>`);
+
+// var restoDivisao = numeroUm % numeroDois;
+// document.write(`O valor da operação é: ${restoDivisao}`);
+
+
+
+// Operações de comparação
+// == -> igual a
+// != -> diferente de
+// < -> menor que
+// > -> maior que
+// <= -> menor ou igual a que 
+// >= -> maior ou igual a que 
+
+document.write("<h3>Operador de compareção: igual a</h3>.")
+var valor1 = 9;
+var valor2 = 9;
+
+if (valor1 == valor2) 
+{
+   document.write(`${valor1} é igual a ${valor2}.`); 
+}else
+{
+    document.write(`${valor1} é diferente de ${valor2}.`);
+}
+
+document.write("<h3>Operador de compareção: diferente de</h3>.")
+var valor1 = 9;
+var valor2 = 8;
+
+if (valor1 != valor2) 
+{
+   document.write(`${valor1} é diferente de ${valor2}.`); 
+}else
+{
+    document.write(`${valor1} é igual a ${valor2}.`);
+}
+
+document.write("<h3>Operador de compareção: menor que</h3>.")
+var valor1 = 7;
+var valor2 = 8;
+
+if (valor1 < valor2) 
+{
+   document.write(`${valor1} é menor que ${valor2}.`); 
+}else
+{
+    document.write(`${valor1} é maior que ${valor2}.`);
+}
+
+document.write("<h3>Operador de compareção: menor ou igual a que</h3>.")
+var valor1 = 9;
+var valor2 = 8;
+
+if (valor1 <= valor2) 
+{
+   document.write(`${valor1} é menor ou igual a ${valor2}.`); 
+}else
+{
+    document.write(`${valor1} é maior que ${valor2}.`);
+}
+
+document.write("<h3>Operador de compareção: maior ou igual a que</h3>.")
+var valor1 = 9;
+var valor2 = 8;
+
+if (valor1 >= valor2) 
+{
+   document.write(`${valor1} é maior ou igual a ${valor2}.`); 
+}else
+{
+    document.write(`${valor1} é menor que ${valor2}.`);
+}
+
+
+
