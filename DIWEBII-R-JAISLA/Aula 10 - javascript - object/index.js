b5373a25d6f23ba92627c90b644d67d7137e0dc2
JaislaCosta <jaislacosta2@gmail.com>
2025-10-15 20:41:43 -0300

    Add files via upload

---
+++ DIWEBII-R-JAISLA/Aula 10 - javascript - object/index.js
@@ -0,0 +1,23 @@
+// var cadeira = {
+//     cor: "Preta",
+//     altura: 118,
+//     largura: 70,
+//     profundidade: 64,
+//     peso: 17
+// };
+
+// cadeira.cor = "Branca";
+// delete cadeira.cor;
+// document.write(`A cor da cadeira é: ${cadeira.cor}<br>`);
+
+// document.write(`A altura da cadeira é: ${cadeira.altura}<br>`);
+// document.write(`A largura da cadeira é: ${cadeira.largura}<br>`);
+// document.write(`A profundidade da cadeira é: ${cadeira.profundidade}<br>`);
+// document.write(`O peso da cadeira é: ${cadeira.peso}`);
+
+var mesa = new Object();
+mesa.cor = "Preta";
+mesa.largura = 150;
+
+document.write(`Cor da mesa: ${mesa.cor}<br>`);
+document.write(`Largura da mesa: ${mesa.largura}cm<br>`);
